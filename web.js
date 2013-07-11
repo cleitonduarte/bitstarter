8435ba3890aa4ad718259fe79d2a9d2781ce8f5c
cleiton-duarte-barbosa <cleiton.dooarte@gmail.com>
2013-07-11 23:17:45 +0000

    include new repo 2

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(2500);
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+  buffer.write(fs.readFileSync('index.html','utf8'));
+
+  response.send(buffer.toString("utf8"));
 });
 
 var port = process.env.PORT || 5000;
