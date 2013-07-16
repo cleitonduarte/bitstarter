03be5e3728ece0246f38cf23801d1fe57b83434e
cleiton-duarte-barbosa <cleiton.dooarte@gmail.com>
2013-07-16 00:34:31 +0000

    HW3 part 3 v2

--- grader.js
+++ grader.js
@@ -25,13 +25,16 @@ var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
+var HTMLFILE_PAGE = "page.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var sys = require('util'),
+    rest = require('./restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -49,8 +52,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
@@ -61,14 +64,32 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-if(require.main == module) {
-    program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
-    exports.checkHtmlFile = checkHtmlFile;
-}
+      if(require.main == module) {
+	  program
+	      .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	      .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	      .option('-u, --url <url_page>', 'URL to page', clone(assertFileExists), HTMLFILE_DEFAULT)
+	      .parse(process.argv);
+
+	    if (program.url){
+		 rest.get(program.url).on('complete', function(result) {
+		  if (result instanceof Error) {
+		  sys.puts('Error: ' + result.message);
+		  this.retry(5000); // try again after 5 sec
+		  } else {
+		    sys.puts(result);
+		   }
+		 });
+		fs.writeFile('/page.html', result, function(err) {
+		  if (err) throw err;
+		  console.log('It\'s saved!');
+		});
+		var checkJson = checkHtmlFile(HTMLFILE_PAGE , program.checks);
+		} else {
+		 var checkJson = checkHtmlFile(program.file, program.checks);
+		}
+	  var outJson = JSON.stringify(checkJson, null, 4);
+	  console.log(outJson);
+      } else {
+	  exports.checkHtmlFile = checkHtmlFile;
+      }
